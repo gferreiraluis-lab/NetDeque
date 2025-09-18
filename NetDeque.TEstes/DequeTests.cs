09f721559d75f132b79e54c0cae038965434ef39
Gustavo Luis <gferreiraluis@outlook.com>
2025-09-17 22:30:33 -0300

    atualizacao fluent assertions

--- NetDeque.TEstes/DequeTests.cs
+++ NetDeque.TEstes/DequeTests.cs
@@ -1,6 +1,7 @@
 using System;
 using NetDeque;
 using Xunit;
+using FluentAssertions;
 
 namespace NetDeque.Tests
 {
@@ -10,8 +11,10 @@ namespace NetDeque.Tests
         public void NovoDeque_DeveComecarVazio()
         {
             var dq = new Deque<int>();
-            Assert.Equal(0, dq.Count);
-            Assert.True(dq.IsEmpty);
+            // Assert.Equal(0, dq.Count);
+            dq.Count.Should().Be(0);
+            // Assert.True(dq.IsEmpty);
+            dq.IsEmpty.Should().BeTrue();
         }
 
         [Fact]
@@ -20,10 +23,14 @@ namespace NetDeque.Tests
             var dq = new Deque<int>();
             dq.AddBeg(10);
 
-            Assert.False(dq.IsEmpty);
-            Assert.Equal(1, dq.Count);
-            Assert.Equal(10, dq.PeekBeg());
-            Assert.Equal(10, dq.PeekEnd());
+            // Assert.False(dq.IsEmpty);
+            dq.IsEmpty.Should().BeFalse();
+            // Assert.Equal(1, dq.Count);
+            dq.Count.Should().Be(1);
+            // Assert.Equal(10, dq.PeekBeg());
+            dq.PeekBeg().Should().Be(10);
+            // Assert.Equal(10, dq.PeekEnd());
+            dq.PeekEnd().Should().Be(10);
         }
 
         [Fact]
@@ -34,10 +41,14 @@ namespace NetDeque.Tests
             dq.AddBeg(2);
             dq.AddBeg(3);
 
-            Assert.Equal(3, dq.RemBeg());
-            Assert.Equal(2, dq.RemBeg());
-            Assert.Equal(1, dq.RemBeg());
-            Assert.True(dq.IsEmpty);
+            // Assert.Equal(3, dq.RemBeg());
+            dq.RemBeg().Should().Be(3);
+            // Assert.Equal(2, dq.RemBeg());
+            dq.RemBeg().Should().Be(2);
+            // Assert.Equal(1, dq.RemBeg());
+            dq.RemBeg().Should().Be(1);
+            // Assert.True(dq.IsEmpty);
+            dq.IsEmpty.Should().BeTrue();
         }
 
         [Fact]
@@ -46,10 +57,14 @@ namespace NetDeque.Tests
             var dq = new Deque<int>();
             dq.AddEnd(10);
 
-            Assert.False(dq.IsEmpty);
-            Assert.Equal(1, dq.Count);
-            Assert.Equal(10, dq.PeekBeg());
-            Assert.Equal(10, dq.PeekEnd());
+            // Assert.False(dq.IsEmpty);
+            dq.IsEmpty.Should().BeFalse();
+            // Assert.Equal(1, dq.Count);
+            dq.Count.Should().Be(1);
+            // Assert.Equal(10, dq.PeekBeg());
+            dq.PeekBeg().Should().Be(10);
+            // Assert.Equal(10, dq.PeekEnd());
+            dq.PeekEnd().Should().Be(10);
         }
 
         [Fact]
@@ -60,17 +75,23 @@ namespace NetDeque.Tests
             dq.AddEnd(2);
             dq.AddEnd(3);
 
-            Assert.Equal(3, dq.RemEnd());
-            Assert.Equal(2, dq.RemEnd());
-            Assert.Equal(1, dq.RemEnd());
-            Assert.True(dq.IsEmpty);
+            // Assert.Equal(3, dq.RemEnd());
+            dq.RemEnd().Should().Be(3);
+            // Assert.Equal(2, dq.RemEnd());
+            dq.RemEnd().Should().Be(2);
+            // Assert.Equal(1, dq.RemEnd());
+            dq.RemEnd().Should().Be(1);
+            // Assert.True(dq.IsEmpty);
+            dq.IsEmpty.Should().BeTrue();
         }
 
         [Fact]
         public void RemoverDoInicio_Vazio_DeveLancarExcecao()
         {
             var dq = new Deque<int>();
-            Assert.Throws<InvalidOperationException>(() => dq.RemBeg());
+            // Assert.Throws<InvalidOperationException>(() => dq.RemBeg());
+            Action act = () => dq.RemBeg();
+            act.Should().Throw<InvalidOperationException>();
         }
 
         [Fact]
@@ -81,17 +102,23 @@ namespace NetDeque.Tests
             dq.AddBeg(2);
             dq.AddBeg(3);
 
-            Assert.Equal(3, dq.RemBeg());
-            Assert.Equal(2, dq.RemBeg());
-            Assert.Equal(1, dq.RemBeg());
-            Assert.True(dq.IsEmpty);
+            // Assert.Equal(3, dq.RemBeg());
+            dq.RemBeg().Should().Be(3);
+            // Assert.Equal(2, dq.RemBeg());
+            dq.RemBeg().Should().Be(2);
+            // Assert.Equal(1, dq.RemBeg());
+            dq.RemBeg().Should().Be(1);
+            // Assert.True(dq.IsEmpty);
+            dq.IsEmpty.Should().BeTrue();
         }
 
         [Fact]
         public void RemoverDoFinal_Vazio_DeveLancarExcecao()
         {
             var dq = new Deque<int>();
-            Assert.Throws<InvalidOperationException>(() => dq.RemEnd());
+            // Assert.Throws<InvalidOperationException>(() => dq.RemEnd());
+            Action act = () => dq.RemEnd();
+            act.Should().Throw<InvalidOperationException>();
         }
 
         [Fact]
@@ -102,12 +129,17 @@ namespace NetDeque.Tests
             dq.AddEnd(2);
             dq.AddEnd(3);
 
-            Assert.Equal(3, dq.RemEnd());
-            Assert.Equal(2, dq.RemEnd());
-            Assert.Equal(1, dq.RemEnd());
-            Assert.True(dq.IsEmpty);
+            // Assert.Equal(3, dq.RemEnd());
+            dq.RemEnd().Should().Be(3);
+            // Assert.Equal(2, dq.RemEnd());
+            dq.RemEnd().Should().Be(2);
+            // Assert.Equal(1, dq.RemEnd());
+            dq.RemEnd().Should().Be(1);
+            // Assert.True(dq.IsEmpty);
+            dq.IsEmpty.Should().BeTrue();
         }
 
-       
+
+
     }
 }
